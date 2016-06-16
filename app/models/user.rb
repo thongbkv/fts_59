1e9565eac830e2a72483792979dab34e95847c87
thongbkv <framgia@framgia0160-lt.framgia.com>
2016-06-16 17:55:06 +0700

    sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,26 @@
 class User < ActiveRecord::Base
   has_many :lessons
   has_many :questions
+
+  before_save {self.email == email.downcase}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+  validates :password, presence: true, length: {minimum: 6}
+
+  has_secure_password
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root "static_pages#index"
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
+
+  resources :users, only: [:new, :create]
 end
