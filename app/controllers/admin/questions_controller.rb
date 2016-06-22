f071bfbd17d4283486ba3c1120aa2cb122909063
thongbkv <framgia@framgia0160-lt.framgia.com>
2016-06-22 17:25:29 +0700

    admin-update-question

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,10 +1,12 @@
 class Admin::QuestionsController < ApplicationController
   before_action :logged_in_user, :require_admin
-  before_action :load_subjects, only: :new
+  before_action :load_subjects, only: [:new, :edit]
+  before_action :load_question, only: [:edit, :update, :destroy]
   layout "admin"
 
   def index
-    @questions = Question.paginate page: params[:page]
+    @questions = current_user.questions.paginate page: params[:page],
+      per_page: Settings.per_page
   end
 
   def new
@@ -23,6 +25,25 @@ class Admin::QuestionsController < ApplicationController
     end
   end
 
+  def update
+    if @question.update_attributes question_params
+      flash[:success] = t "update_question_success"
+      redirect_to admin_questions_path
+    else
+      flash[:danger] = t "update_question_fail"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @question.destroy
+      flash[:success] = t "delete_question_success"
+    else
+      flash[:danger] = t "delete_question_fail"
+    end
+    redirect_to admin_questions_path
+  end
+
   private
   def question_params
     params.require(:question).permit :content, :question_type, :state, :subject_id,
@@ -31,5 +52,9 @@ class Admin::QuestionsController < ApplicationController
 
   def load_subjects
     @subjects = Subject.all
+  end
+
+  def load_question
+    @question = Question.find_by id: params[:id]
   end
 end
